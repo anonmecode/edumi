42fe9e67c09d56128e0e10e30043716e35959aaa
anonmecode <anonmetube@gmail.com>
2024-06-10 18:44:42 +0530

    parse error fixed
    There was an extra setup.php file with incomplete code located at theme_uri('/includes/front/setup.php'). Actual file location was theme_uri('/includes/setup.php').

--- functions.php
+++ functions.php
@@ -10,4 +10,4 @@ include(get_theme_file_path('/includes/setup.php'));
 // Hooks
 add_action('wp_head', 'edu_head', 5);
 add_action('wp_enqueue_scripts', 'edu_enqueue');
-// add_action('after_setup_theme', 'edu_setup_theme');
+add_action('after_setup_theme', 'edu_setup_theme');
